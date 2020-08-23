35aade3a4eae75a340f9592a0b0843f3b866e3c4
junlinp <junlinp@qq.com>
2020-08-23 19:19:28 +0800

    add multipler update

--- src/openMVG/sfm/sfm_data_BA_processor.cpp
+++ src/openMVG/sfm/sfm_data_BA_processor.cpp
@@ -1,4 +1,5 @@
 #include "sfm_data_BA_processor.hpp"
+
 #include "ceres/problem.h"
 
 ceres::CostFunction* IntrinsicsToCostFunction(
@@ -20,7 +21,7 @@ ceres::CostFunction* IntrinsicsToCostFunction(
     case openMVG::cameras::EINTRINSIC::PINHOLE_CAMERA_FISHEYE:
       return openMVG::sfm::ResidualErrorFunctor_Pinhole_Intrinsic_Fisheye::
           Create(observation, weight);
-    //case openMVG::cameras::EINTRINSIC::CAMERA_SPHERICAL:
+    // case openMVG::cameras::EINTRINSIC::CAMERA_SPHERICAL:
     //  return openMVG::sfm::ResidualErrorFunctor_Intrinsic_Spherical::Create(
     //     intrinsic, observation, weight);
     default:
@@ -31,66 +32,106 @@ ceres::CostFunction* IntrinsicsToCostFunction(
 namespace openMVG {
 namespace sfm {
 
+struct MultiplerCost {
+  std::vector<double> y, z;
+  double rho;
+
+  MultiplerCost(std::vector<double> y, std::vector<double> z, double rho_) {
+    y = y;
+    z = z;
+    rho = rho_;
+  }
+
+  template <class T>
+  bool operator()(const T* x, T* residual) const {
+    const size_t vector_size = y.size();
+    double d = 1.0 / rho;
+    for (int i = 0; i < vector_size; i++) {
+      residual[0] += T(y[i]) * (x[i] - T(z[i])) +
+                     d * (x[i] - T(z[i])) * (x[i] - T(z[i]));
+    }
+    return true;
+  }
+};
 
 void CPUProcessor::OptimizeParameters() {
-    /*
-    std::cout << "Origin Camera : " << std::endl; 
-    for (auto& camera_para : camera_params_) {
-        std::cout << camera_para << " ";
-    }
-    std::cout << std::endl;
+  /*
+  std::cout << "Origin Camera : " << std::endl;
+  for (auto& camera_para : camera_params_) {
+      std::cout << camera_para << " ";
+  }
+  std::cout << std::endl;
 
-    std::cout << "Origin pose : " << std::endl; 
-    for (auto& camera_para : pose_params_) {
-        std::cout << camera_para << " ";
-    }
-    std::cout << std::endl;
+  std::cout << "Origin pose : " << std::endl;
+  for (auto& camera_para : pose_params_) {
+      std::cout << camera_para << " ";
+  }
+  std::cout << std::endl;
 
-    std::cout << "Origin structure : " << std::endl; 
-    for (auto& camera_para : structure_params_) {
-        std::cout << camera_para << " ";
-    }
-    std::cout << std::endl;
-    */
-    ceres::CostFunction* intrins_cost_function = IntrinsicsToCostFunction(camera_type_, Eigen::Map<const Eigen::Vector2d>(&observations_[0]), structure_weight_);
-    ceres::Problem problem;
+  std::cout << "Origin structure : " << std::endl;
+  for (auto& camera_para : structure_params_) {
+      std::cout << camera_para << " ";
+  }
+  std::cout << std::endl;
+  */
+  ceres::CostFunction* intrins_cost_function = IntrinsicsToCostFunction(
+      camera_type_, Eigen::Map<const Eigen::Vector2d>(&observations_[0]),
+      structure_weight_);
+  ceres::Problem problem;
 
-    problem.AddParameterBlock(&camera_params_[0], camera_params_.size());
-    if (options_.intrinsics_opt != openMVG::cameras::Intrinsic_Parameter_Type::ADJUST_ALL) {
-        problem.SetParameterBlockConstant(&camera_params_[0]);
-    }
-    problem.AddParameterBlock(&pose_params_[0], pose_params_.size());
-    if (options_.extrinsics_opt != openMVG::sfm::Extrinsic_Parameter_Type::ADJUST_ALL) {
-        problem.SetParameterBlockConstant(&pose_params_[0]);
-    }
-    problem.AddParameterBlock(&structure_params_[0], structure_params_.size());
-    if (options_.structure_opt != openMVG::sfm::Structure_Parameter_Type::ADJUST_ALL) {
-        problem.SetParameterBlockConstant(&structure_params_[0]);
-    }
-    problem.AddResidualBlock(intrins_cost_function, nullptr, &camera_params_[0], &pose_params_[0], &structure_params_[0]);
-    ceres::Solver::Options options; 
-    ceres::Solver::Summary summary;
-    ceres::Solve(options, &problem, &summary);
-    std::cout << summary.BriefReport() << std::endl;
-    /*
-    std::cout << "Camera : " << std::endl; 
-    for (auto& camera_para : camera_params_) {
-        std::cout << camera_para << " ";
-    }
-    std::cout << std::endl;
+  problem.AddParameterBlock(&camera_params_[0], camera_params_.size());
+  ceres::CostFunction* pose_multipler_cost =
+      new ceres::AutoDiffCostFunction<MultiplerCost, 1, 6>(new MultiplerCost(
+          pose_params_multipler_, pose_consensus_params_, rho_));
+  ceres::CostFunction* structure_multipler_cost =
+      new ceres::AutoDiffCostFunction<MultiplerCost, 1, 3>(new MultiplerCost(
+          structure_params_multipler_, structure_consensus_params_, rho_));
 
-    std::cout << "pose : " << std::endl; 
-    for (auto& camera_para : pose_params_) {
-        std::cout << camera_para << " ";
-    }
-    std::cout << std::endl;
+  if (options_.intrinsics_opt !=
+      openMVG::cameras::Intrinsic_Parameter_Type::ADJUST_ALL) {
+    problem.SetParameterBlockConstant(&camera_params_[0]);
+  }
 
-    std::cout << "structure : " << std::endl; 
-    for (auto& camera_para : structure_params_) {
-        std::cout << camera_para << " ";
-    }
-    std::cout << std::endl;
-    */
+  problem.AddParameterBlock(&pose_params_[0], pose_params_.size());
+  if (options_.extrinsics_opt !=
+      openMVG::sfm::Extrinsic_Parameter_Type::ADJUST_ALL) {
+    problem.SetParameterBlockConstant(&pose_params_[0]);
+  }
+
+  problem.AddParameterBlock(&structure_params_[0], structure_params_.size());
+  if (options_.structure_opt !=
+      openMVG::sfm::Structure_Parameter_Type::ADJUST_ALL) {
+    problem.SetParameterBlockConstant(&structure_params_[0]);
+  }
+  problem.AddResidualBlock(intrins_cost_function, nullptr, &camera_params_[0],
+                           &pose_params_[0], &structure_params_[0]);
+  problem.AddResidualBlock(pose_multipler_cost, nullptr, &pose_params_[0]);
+  problem.AddResidualBlock(structure_multipler_cost, nullptr,
+                           &structure_params_[0]);
+
+  ceres::Solver::Options options;
+  ceres::Solver::Summary summary;
+  ceres::Solve(options, &problem, &summary);
+  std::cout << summary.BriefReport() << std::endl;
+  /*
+  std::cout << "Camera : " << std::endl;
+  for (auto& camera_para : camera_params_) {
+      std::cout << camera_para << " ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "pose : " << std::endl;
+  for (auto& camera_para : pose_params_) {
+      std::cout << camera_para << " ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "structure : " << std::endl;
+  for (auto& camera_para : structure_params_) {
+      std::cout << camera_para << " ";
+  }
+  std::cout << std::endl;
+  */
 }
 }  // namespace sfm
 }  // namespace openMVG
--- src/openMVG/sfm/sfm_data_BA_processor.hpp
+++ src/openMVG/sfm/sfm_data_BA_processor.hpp
@@ -22,7 +22,14 @@ class Processor {
         observations_(observation),
         options_(options),
         structure_weight_(structure_weight),
-        rho_(rho) {}
+        rho_(rho),
+        camera_params_multipler_(camera_params.size(), 0.0),
+        pose_params_multipler_(pose_params.size(), 0.0),
+        structure_params_multipler_(structure_params.size(), 0.0),
+        camera_consensus_params_(camera_params.size(), 0.0),
+        pose_consensus_params_(pose_params.size(), 0.0),
+        structure_consensus_params_(structure_params.size(), 0.0)
+         {}
   virtual ~Processor() {}
   virtual void OptimizeParameters() = 0;
   // TODO: junlinp@qq.com
@@ -71,36 +78,79 @@ class CPUProcessor : public Processor {
   void OptimizeParameters() override;
 
   std::vector<double> getLocalCameraParams() override {
-    Eigen::VectorXd x = Eigen::Map<Eigen::VectorXd>(&camera_params_[0], camera_params_.size());
-    Eigen::VectorXd z = Eigen::Map<Eigen::VectorXd>(&camera_params_[0], camera_params_.size());
-    return camera_params_multipler_;
+    Eigen::VectorXd x =
+        Eigen::Map<Eigen::VectorXd>(&camera_params_[0], camera_params_.size());
+    Eigen::VectorXd y = Eigen::Map<Eigen::VectorXd>(
+        &camera_params_multipler_[0], camera_params_.size());
+    Eigen::VectorXd res = (x - 1.0 / rho_ * y);
+    std::vector<double> res_vector;
+    for (int i = 0; i < camera_params_.size(); i++) {
+      res_vector.push_back(res(i));
+    }
+    return res_vector;
   };
 
   std::vector<double> getLocalPoseParams() override {
-    return pose_params_multipler_;
+    return ZUpdate(pose_params_, pose_params_multipler_, rho_);
   };
 
   std::vector<double> getLocalStructureParams() override {
-    return structure_params_multipler_;
+    return ZUpdate(structure_params_, structure_params_multipler_, rho_);
   };
 
   void setUpdateCameraParamsConsusence(
       const std::vector<double>& camera_consensus_params) override {
     assert(camera_consensus_params.size() == camera_consensus_params_.size());
     camera_consensus_params_ = camera_consensus_params;
+    std::vector<double> delta_y = YUpdate(camera_params_, camera_consensus_params_, rho_);
+    for (int i = 0; i < camera_params_multipler_.size(); i++) {
+      camera_params_multipler_[i] += delta_y[i];
+    }
   }
 
   virtual void setUpdatePoseParamsConsusence(
       const std::vector<double>& pose_consensus_params) override {
     assert(pose_consensus_params.size() == pose_consensus_params_.size());
     pose_consensus_params_ = pose_consensus_params;
+    std::vector<double> delta_y = YUpdate(pose_params_, pose_consensus_params_, rho_);
+    for (int i = 0; i < pose_params_multipler_.size(); i++) {
+      pose_params_multipler_[i] += delta_y[i];
+    }
   }
 
   virtual void setUpdateStructureParams(
-      const std::vector<double>& structure_consensus_params) {
+      const std::vector<double>& structure_consensus_params) override {
     assert(structure_consensus_params.size() ==
            structure_consensus_params_.size());
     structure_consensus_params_ = structure_consensus_params;
+    std::vector<double> delta_y = YUpdate(structure_params_, structure_consensus_params_, rho_);
+    for(int i = 0; i < structure_params_multipler_.size(); i++) {
+      structure_params_multipler_[i] += delta_y[i];
+    }
+  }
+
+ private:
+  std::vector<double> ZUpdate(std::vector<double> origin_params,
+                              std::vector<double> origin_params_multipler,
+                              double rho) {
+    Eigen::VectorXd x =
+        Eigen::Map<Eigen::VectorXd>(&origin_params[0], origin_params.size());
+    Eigen::VectorXd y = Eigen::Map<Eigen::VectorXd>(
+        &origin_params_multipler[0], origin_params_multipler.size());
+    Eigen::VectorXd res = (x - 1.0 / rho_ * y);
+    std::vector<double> res_vector;
+    for (int i = 0; i < origin_params.size(); i++) {
+      res_vector.push_back(res(i));
+    }
+    return res_vector;
+  }
+  std::vector<double> YUpdate(std::vector<double>& origin_params, std::vector<double>& origin_consusens_params, double rho) {
+    std::vector<double> res;
+    for (int i = 0; i < origin_params.size();i++) {
+      res.push_back(rho * (origin_params[i] - origin_consusens_params[i]));
+    }
+    return res;
+
   }
 };
 
